7b171d6ffec84aaae1bd0b5ca57f2a7dfbf3b36e
hannnnk1231 <cwhuangb1321@gmail.com>
2019-10-14 00:53:58 +0800

    1014 update

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const zombiefeed = artifacts.require("ZombieFeeding");
-const zombiefactory = artifacts.require("ZombieFactory");
-
-module.exports = function(deployer) {
-  deployer.deploy(zombiefactory);
-  deployer.link(zombiefactory, zombiefeed);
-  deployer.deploy(zombiefeed);
-};
